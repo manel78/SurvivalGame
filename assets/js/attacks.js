c3fae2acee4dcc231ce615589a81d530810f57fd
emanel <manel.elhadi@ynov.com>
2024-04-19 10:50:57 +0200

    ajout variable

--- assets/js/attacks.js
+++ assets/js/attacks.js
@@ -111,3 +111,49 @@ class Joueur {
         }
     }
 }
+function collision(projectile, ennemi) {
+    function collision(projectile, ennemi) {
+        //  attaque de pres ou a distance 
+        if (projectile instanceof Projectile) { 
+            // verife la distances 
+             let distance = Math.sqrt(Math.pow(projectile.position.x - ennemi.position.x, 2) + Math.pow(projectile.position.y - ennemi.position.y, 2));
+            
+            // > alors collision
+            let rayonCollisionDistance = 10; // à adapter pour les attaques à distance
+            return distance <= rayonCollisionDistance;
+        } else if (projectile instanceof Ennemi) {
+            // attaque proche 
+            let distance = Math.sqrt(Math.pow(projectile.position.x - ennemi.position.x, 2) + Math.pow(projectile.position.y - ennemi.position.y, 2));
+            
+            // > alors collision
+            let rayonCollisionPres = 20; // à adapter pour les attaques de près
+            return distance <= rayonCollisionPres;
+        }
+    }
+    
+}
+
+function limiteChampDeJeu(projectile) {
+     // limite du jeu hauteur et longeurs 
+     let limiteXMin = 0;  // changer les mesures 
+     let limiteXMax = 800; 
+     let limiteYMin = 0;
+     let limiteYMax = 600; 
+     
+     // projectiles en dehors du jeu ?
+     return (projectile.position.x < limiteXMin || projectile.position.x > limiteXMax || projectile.position.y < limiteYMin || projectile.position.y > limiteYMax);
+ 
+}
+
+// Utilisation des armes 
+let epee = new Arme("Épée", "pres", 20);
+let arcNormal = new Arme('ArcNormal', "loin", 25);
+
+let joueur = new Joueur("Joueur", 15, 10, 100, epee, arcNormal);
+
+let mutantPres = new Ennemi("Mutant", 8, 3, 50, "pres"); 
+let mutantLoin = new Ennemi("Mutant", 12, 5, 80, "loin"); 
+
+joueur.attaquerPres(mutantPres); 
+joueur.attaquerLoin(mutantLoin, {x: 1, y: 0.5}, 10); // ex de direction et de vitesse du projectile
+message.txt
